1e5eb3a0f4c1cfc3d4bd9b79e754f07747a7ffa3
Yan Chen <yanche.belongs@gmail.com>
2017-12-22 16:33:12 -0800

    entry init

---
+++ src/entry.ts
@@ -0,0 +1,12 @@
+
+import { } from "@belongs/compiler/src/language/tlang"
+
+const tlangEditor = document.getElementById("tlang-editor");
+const mipsEditor = document.getElementById("mips-editor");
+const btnCompile = document.getElementById("btn-compile");
+const btnRun = document.getElementById("btn-run");
+const clickEvtName = "click";
+
+btnCompile.addEventListener(clickEvtName, ()=>{
+    
+});
